b3529b08e55fa2518d24fb84031ff8ec00c8a849
Arsalan Kazmi <sonicspeed848@gmail.com>
2021-03-30 13:05:41 +0000

    Add a command line option to skip the doas/sudo check

--- violence.cpp
+++ violence.cpp
@@ -2,37 +2,61 @@
 #include <fstream>
 #include <stdlib.h>
 #include <string>
+#include <string.h>
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
 	// variable stuff
-	bool violence;
+	bool violence, sudoascheck;
 	string command, sudoas, violentcmd;
-	violence = true;
+	violence, sudoascheck = true, true;
 	sudoas = "doas";
+
+    if (argc > 1)
+    {
+        cout << argv[1] << endl;
+        if (strcmp(argv[1], "--skip") == 0)
+        {
+            // ignore the doas/sudo check
+            cout << "doas/sudo check will be skipped" << endl;
+            sudoascheck = false;
+        }
+    }
 	
 	// check if the system has doas or sudo (in that order)
 	ifstream doas("sudo");
-	if (!doas)
-	{
-		cout << "doas not found -- falling back to sudo" << endl;
-		ifstream sudo("sudo");
-		if (!sudo)
-		{
-			cout << "sudo not found -- program terminated" << endl;
-			exit(EXIT_FAILURE);
-		}
-		else
-		{
-			sudoas = "sudo";
-		}
-        sudo.close();
-	}
+    if (sudoascheck)
+    {
+        if (!doas)
+        {
+            cout << "doas not found -- falling back to sudo" << endl;
+            ifstream sudo("sudo");
+            if (!sudo)
+            {
+                cout << "sudo not found -- program terminated" << endl;
+                exit(EXIT_FAILURE);
+            }
+            else
+            {
+                sudoas = "sudo ";
+            }
+            sudo.close();
+        }
+        else
+        {
+            sudoas = "doas ";
+        }
+    }
+    else
+    {
+        sudoas = "";
+    }
 	doas.close();
 	
 	// main loop
 	cout << "Violence ENABLED." << endl;
+    violence = true;
 	while (violence)
 	{
 		cout << "violence> ";
@@ -43,7 +67,7 @@ int main()
 		}
 		else
 		{
-			violentcmd = sudoas + " " + command; // for example: doas apt install some-package
+			violentcmd = sudoas + command; // for example: doas apt install some-package
 			cout << violentcmd << endl;
 			system(violentcmd.c_str());
 		}
